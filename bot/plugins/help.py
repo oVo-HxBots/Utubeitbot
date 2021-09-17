d2a863892c0b79b8a85d8033ecb76039a3757b9d
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-09-17 20:14:02 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -20,7 +20,7 @@ def map_btns(pos):
         url = auth.GetAuthUrl()
         button = [
             [InlineKeyboardButton(text="<--", callback_data=f"help+{pos-1}")],
-            [InlineKeyboardButton(text="Login", callback_data="{command.login}")],
+            [InlineKeyboardButton(text="Login", callback_data=f"{command.login}")],
         ]
     else:
         button = [
