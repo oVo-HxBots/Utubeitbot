fde44b4558e210eacd8b435becd2f1ff166f5ce1
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-09-17 06:44:45 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -36,7 +36,6 @@ def map_btns(pos):
     Filters.private
     & Filters.incoming
     & Filters.command("help")
-    & Filters.user(Config.AUTH_USERS)
 )
 async def _help(c: UtubeBot, m: Message):
     await m.reply_chat_action("typing")
