9707c9a4fc8f97804a248530a9dedff998258473
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-05 15:15:05 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -4,7 +4,7 @@ from .config import Config
 class UtubeBot(Client):
     def __init__(self):
         super().__init__(
-            session_name=Utubeitbot,
+            session_name='Utubeitbot',
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
