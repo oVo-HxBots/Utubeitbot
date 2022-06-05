b8827ca977a09d4670acbfdc76529e01881dadd9
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-05 16:29:31 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -1,15 +1,15 @@
-import logging from pyrogram 
-import filters as Filters 
+import logging from pyrogram
+import filters as Filters
 from ..utubebot import UtubeBot
-from ..config import Config 
+from ..config import Config
 
-log = logging.getLogger(__name__) 
+log = logging.getLogger(__name__)
 
-@UtubeBot.on_message( 
-  Filters.private 
-  & Filters.incoming 
+@UtubeBot.on_message(
+  Filters.private
+  & Filters.incoming
   & ~Filters.user(Config.AUTH_USERS)
 )
-async def _non_auth_usr_msg(c, m): 
-  await m.delete(True) 
+async def _non_auth_usr_msg(c, m):
+  await m.delete(True)
   log.info(f"{Config.AUTH_USERS} Unauthorised user {m.chat} contacted. Message {m} deleted!!")
