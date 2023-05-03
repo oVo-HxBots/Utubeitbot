c761b21249e4dd5e63e20013727630205c07415a
Kamlesh Kumar Saini <131683941+BrothersIndia7@users.noreply.github.com>
2023-05-03 21:33:34 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -26,7 +26,7 @@ async def _start(c: UtubeBot, m: Message):
                       InlineKeyboardButton("Support Group", url="https://t.me/HxSupport")
                   ],
                   [  
-                      InlineKeyboardButton("Upgrade😀", callback_data="/help")
+                      InlineKeyboardButton("Upgrade😀", url="https://google.com")
                   ]]
         ),
     )
