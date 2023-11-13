4be962b40c9ae11e8daba93dbdb0eaee7283c94c
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-13 16:56:01 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -14,7 +14,7 @@ from ..helpers.database import db
 )
 async def _start(c: UtubeBot, m: Message):
     await m.reply_chat_action("typing")
-    user = message.from_user
+    user = m.from_user
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id)
     await m.reply_text(
