08c9193a9bc1361076964c75ef357554efe0ba31
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-14 19:27:46 +0530

    Update force_sub.py

--- bot/plugins/force_sub.py
+++ bot/plugins/force_sub.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from ..config import Config
-from helpers.database import db
+from ..helpers.database import db
 
 async def not_subscribed(c: UtubeBot, m: Message):
     await db.add_user(user.id)
