3b18da62446a54bf0ae121dc6e7a2af4bb0c7788
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-16 17:46:28 +0530

    Update translations.py

--- bot/translations.py
+++ bot/translations.py
@@ -53,11 +53,11 @@ class Messages:
 
     UPDATE_MSG = [
         ".",
-        f"**         Date:03-05-2023; Wednesday \n\n__• Created Upgrade Option, Fixed some minor bugs.__**",
+        f"**         Date:03-05-2023; Wednesday \n\n__• Created Upgrade Option.\n• Fixed some minor bugs.__**",
         f"**         Date:06-12-2022; Tuesday \n\n__• Added Render Support and fixes some issues.__**", 
         f"**         Date:04-11-2022; Friday \n\n__• Updated Code to support on Koyeb.com.__**",
         f"**         Date:14-07-2022; Thrusday \n\n__• Added Option for Private Use.__**",
-        f"**         Date:06-06-2022; Monday\n\n__• __Fixed 400 AUTH_BYTES_INVALID: The authorization bytes are invalid__, __session_name error in deployment__, unknown error about markdown__.**",  
+        f"**         Date:06-06-2022; Monday\n\n__• Fixed 400 AUTH_BYTES_INVALID: The authorization bytes are invalid.\n• session_name error in deployment.\n• Unknown error about markdown__.**",  
         "**Bot Developed by [@oVo-HxBots](https://github.com/oVo-HxBots)\n Bot Owned by @HxBots**"
     ]
 
