d5d3e6285f2a862069f1c63495b9b21f25c3d796
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-16 18:31:10 +0530

    Create utils.py

---
+++ bot/plugins/utils.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+from os import execl
+from time import sleep
+from sys import executable
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, RPCError
+from ..config import DOWNLOAD_DIRECTORY, BOT_OWNER
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+@UtubeBot.on_message(filters.private & filters.incoming & filters.command(['log']) & filters.user(BOT_OWNER), group=2)
+def _send_log(c: UtubeBot, m: Message):
+  with open('log.txt', 'rb') as f:
+    try:
+      client.send_document(
+        message.chat.id,
+        document=f,
+        file_name=f.name,
+        reply_to_message_id=m.message_id
+        )
+      LOGGER.info(f'Log file sent to {m.from_user.id}')
+    except FloodWait as e:
+      sleep(e.x)
+    except RPCError as e:
+      message.reply_text(e, quote=True)
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['restart']) & filters.user(BOT_OWNER), group=2)
+def _restart(c: UtubeBot, m: Message):
+  shutil.rmtree(DOWNLOAD_DIRECTORY)
+  LOGGER.info('Deleted DOWNLOAD_DIRECTORY successfully.')
+  message.reply_text('**♻️Restarted Successfully !**', quote=True)
+  LOGGER.info(f'{message.from_user.id}: Restarting...')
+  execl(executable, executable, "-m", "bot")
