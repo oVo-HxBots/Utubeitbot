2057b9c8e0700ce865ca4f26a2804e632dd5be04
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-17 16:55:19 +0530

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -27,9 +27,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def _send_log(c: UtubeBot, m: Message):
   with open('log.txt', 'rb') as f:
     try:
-      await UtubeBot.send_document("self", "log.txt", caption="document caption")
-    else:
-      UtubeBot.send_document(
+       UtubeBot.send_document(
+        self,
         chat_id=m.chat.id,
         document=f,
         file_name=f.name,
