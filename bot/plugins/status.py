1a463d7c220f3574d90a5268702657ecfa50bcb0
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-17 17:18:25 +0530

    Update status.py

--- bot/plugins/status.py
+++ bot/plugins/status.py
@@ -7,14 +7,15 @@ import sys
 import time
 import traceback
 import logging
-
-# import psutil
+import datetime
+import psutil
 
 from ..config import Config
 from ..utubebot import UtubeBot
 from ..translations import Messages as tr
 from pyrogram import filters as Filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from ..helpers.database import db
 
 log = logging.getLogger(__name__)
 
@@ -29,14 +30,20 @@ async def stats_message_fn(c: UtubeBot, m: Message):
     restart_time = Config.BOT_START_DATETIME
     hr, mi, se = map(time_format, up_time(time.time() - Config.BOT_START_TIME))
     total, used, free = shutil.disk_usage(".")
-    # ram = psutil.virtual_memory().percent
-    # cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    cpu = psutil.cpu_percent()
     total = humanbytes(total)
     used = humanbytes(used)
     free = humanbytes(free)
-    # sent = humanbytes(psutil.net_io_counters().bytes_sent)
-    # recv = humanbytes(psutil.net_io_counters().bytes_recv)
-
+    sent = humanbytes(psutil.net_io_counters().bytes_sent)
+    recv = humanbytes(psutil.net_io_counters().bytes_recv)
+    total_users = await db.total_users_count()
+    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - Config.BOT_UPTIME))    
+    start_t = time.time()
+    st = await m.reply('**Aᴄᴄᴇꜱꜱɪɴɢ Tʜᴇ Dᴇᴛᴀɪʟꜱ.....**')    
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    
     msg = (
         f"<b>Bot Current Status</b>\n\n"
         f"<b>Restarted on {restart_time}</b>\n"
@@ -44,10 +51,10 @@ async def stats_message_fn(c: UtubeBot, m: Message):
         f"<b>Total disk space:</b> {total}\n"
         f"<b>Used :</b> {used}\n"
         f"<b>Free :</b> {free}\n"
-        # f"<b>RAM Usage:</b> {ram}%\n"
-        # f"<b>CPU Usage:</b> {cpu}%\n"
-        # f"<b>Downloaded Data:</b> {recv} 🔻\n"
-        # f"<b>Uploaded Data:</b> {sent} 🔺"
+        f"<b>RAM Usage:</b> {ram}%\n"
+        f"<b>CPU Usage:</b> {cpu}%\n"
+        f"<b>Downloaded Data:</b> {recv} 🔻\n"
+        f"<b>Uploaded Data:</b> {sent} 🔺"
     )
     await m.reply_text(
         text=msg,
