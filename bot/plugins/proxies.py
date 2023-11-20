a753d1e465ab4849f462e2ce017e607ba93a73c7
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-20 10:51:48 +0530

    Create proxies.py

---
+++ bot/plugins/proxies.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+import requests
+
+from ..config import Config
+from ..utubebot import UtubeBot
+from ..translations import Messages as tr
+from pyrogram import filters as Filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from ..helpers.database import db
+
+
+
+@UtubeBot.on_message(
+    Filters.private
+    & Filters.incoming
+    & Filters.command("proxies", "py")
+)
+async def proxies(c: UtubeBot, m: Message):
+    username = "1owsgh8w5iux12r"
+    password = "3atwlzo7997woep"
+    proxy = "rp.proxyscrape.com:6060"
+    proxy_auth = "{}:{}@{}".format(username, password, proxy)
+    proxies = {
+            "http":"http://{}".format(proxy_auth),
+            "https":"https://{}".format(proxy_auth)
+    }
+    urlToGet = "http://ip-api.com/json"
+    r = requests.get(urlToGet , proxies=proxies)
+    print("Response:\n{}".format(r.text))
+    await m.reply_text(
+        text="Proxies:\n{}".format(r.text),
+        quote=True
+    )
