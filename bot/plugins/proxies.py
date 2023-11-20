df07e639060794cb2027bf395b7317d2d2d09366
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-20 16:35:10 +0530

    Update proxies.py

--- bot/plugins/proxies.py
+++ bot/plugins/proxies.py
@@ -31,6 +31,7 @@ async def proxies(c: UtubeBot, m: Message):
     PY_DATA = r.json()
     
     await m.reply_text(
-        text="Country:{}.formate(r.json(country))",
+        text=f'IP: {r.json().get("ip")}'
+             f'Country: {r.json().get("country")},
         quote=True
     )
