fb5036572df6fc49c4734780b59e3777f093ef75
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-11-20 11:25:58 +0530

    Update proxies.py

--- bot/plugins/proxies.py
+++ bot/plugins/proxies.py
@@ -30,6 +30,10 @@ async def proxies(c: UtubeBot, m: Message):
     r = requests.get(urlToGet , proxies=proxies)
     print("Response:\n{}".format(r.text))
     await m.reply_text(
-        text="Proxies:\n{}".format(r.json().get("status")),
+        text={
+            "Status:{}".format(r.json().get("status")),
+            "Country:{}".format(r.json().get("country")),
+            "ISP:{}".format(r.json().get("isp")),
+            "IP:{}".format(r.json().get("query")),
         quote=True
     )
