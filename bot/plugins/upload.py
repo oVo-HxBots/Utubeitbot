6e77ebbda16c98d43e3d8660f9e3914eb530a459
Kamlesh Kumar Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2023-12-02 10:42:46 +0530

    Update upload.py

--- bot/plugins/upload.py
+++ bot/plugins/upload.py
@@ -44,7 +44,7 @@ async def _upload(c: UtubeBot, m: Message):
         await m.reply_text(tr.NOT_A_VALID_MEDIA_MSG, True)
         return
 
-    if c.counter >= 6:
+    if c.counter >= 10:
         await m.reply_text(tr.DAILY_QOUTA_REACHED, True)
 
     snt = await m.reply_text(tr.PROCESSING, True)
