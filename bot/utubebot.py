d2e20ab761605d347ed10a287cf6c6802f522a57
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-01-17 18:25:45 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -5,7 +5,7 @@ from .config import Config
 
 class UtubeBot(Client):
     def __init__(self):
-        super().__init__('utubebot,
+        super().__init__('utubebot',
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
