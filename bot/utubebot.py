b418b5e842bfe0a4c745d84385b8cbcfdf7d1904
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-01-18 12:37:25 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -4,13 +4,15 @@ from .config import Config
 
 
 class UtubeBot(Client):
-            def __init__(self):
-            'bot',
+    def __init__(self):
+        super().__init__(
+            name=Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             plugins=dict(root="bot.plugins"),
             workers=6,
-            self.DOWNLOAD_WORKERS = 6,
-            self.counter = 0,
-            self.download_controller = {}
+        )
+        self.DOWNLOAD_WORKERS = 6
+        self.counter = 0
+        self.download_controller = {}
